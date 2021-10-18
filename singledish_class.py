0d41274ecd06e042c31621ea6228c56055e343c2
Hyeong-Sik Yun <89850069+HyeongSikYun@users.noreply.github.com>
2021-10-18 20:38:05 +0900

    Add files via upload

--- singledish_class.py
+++ singledish_class.py
@@ -1,6 +1,12 @@
 import numpy as np
 from astropy.io import fits
+from astropy.wcs import WCS
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import copy
+
+def what_do_you_want_to_see(velo,spec):
+    return np.amax(spec)
 
 def read_fits(dirc,file_name):
   print 'reading ' + file_name + ' at ' + dirc
@@ -22,16 +28,43 @@ def read_ascii(dirc,file_name,skip_num=0):
   return val1, val2
 
 def define_vspace(hdr):
-  coords = read_coord_info(hdr)
-  min_vel = (coords[9]-(coords[10]-1)*coords[11])*1.0e-3
-  max_vel = (coords[9]+(coords[8]-coords[10])*coords[11])*1.0e-3
-  vspace = np.linspace(min_vel,max_vel,coords[8])
+  min_vel = (hdr['CRVAL3']-(hdr['CRPIX3']-1)*hdr['CDELT3'])*1.0e-3
+  max_vel = (hdr['CRVAL3']+(hdr['NAXIS3']-hdr['CRPIX3'])*hdr['CDELT3'])*1.0e-3
+  vspace = np.linspace(min_vel,max_vel,hdr['NAXIS3'])
   return vspace
 
-def show_imshow(data,min_val=None,max_val=None):
+def produce_map_fig(hdu):
+  vspace = define_vspace(hdu[0].header)
+  cube = hdu[0].data
+  new_hdr = copy.deepcopy(hdu[0].header)
+  new_hdr['NAXIS'] = 2
+  del new_hdr['NAXIS3']
+  del new_hdr['NAXIS4']
+  del new_hdr['CTYPE3']
+  del new_hdr['CRVAL3']
+  del new_hdr['CDELT3']
+  del new_hdr['CRPIX3']
+  del new_hdr['CROTA3']
+  del new_hdr['CTYPE4']
+  del new_hdr['CRVAL4']
+  del new_hdr['CDELT4']
+  del new_hdr['CRPIX4']
+  del new_hdr['CROTA4']
+  wcs = WCS(new_hdr)
+  sz = np.shape(cube)
+  map_mat = np.zeros([sz[2],sz[3]])
+  for i in range(sz[3]):
+    for j in range(sz[2]):
+      value = what_do_you_want_to_see(vspace,cube[0,:,j,i])
+      map_mat[j,i] = value
+  
   fig = plt.figure(501)
-  show = plt.imshow(data, origin='lower',vmin=min_val,vmax=max_val)
-  plt.colorbar(show)
+  fig_layout = gridspec.GridSpec(1,1)
+  sub_fig = fig.add_subplot(fig_layout[0,0],projection=wcs)
+  map_img = plt.imshow(map_mat)
+  plt.colorbar(map_img)
+  plt.xlabel('R.A. (J2000)')
+  plt.ylabel('Dec. (J2000)')
   plt.show()
   plt.close(501)
   
